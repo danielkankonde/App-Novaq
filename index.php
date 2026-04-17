3ff28d8a05c689d3fc1ab7c67dca8881abaf5de2
Daniel <danielkankonde@gmail.com~>
2026-04-17 17:43:23 -0400

    test de bug

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 const CI_VERSION = '3.1.13';
 $autoload = __DIR__ . '/vendor/autoload.php';
